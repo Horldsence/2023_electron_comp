1c823ab5066fed08b6d49c9eb572d8c933b496e6
Horldsence <m18511047688@163.com>
2024-05-24 16:19:26 +0800

    12

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from procFunc.mathAssoc import mathProc
 from procFunc.yolov5Proc import detect_img
 from basicFunc.onnxLibrary import ObjectDetector
 
-detector = ObjectDetector('model.onnx')
+detector = ObjectDetector('yoloModel')
 
 
 getImg = Imget()
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         cv2.waitKey(3)
 
         # 使用nanodet模型检测
-        raw_result = detector.run_onnx_model(img)
+        raw_result = detector.run_onnx_model(img)*
         boxes, scores, classes = detector.apply_nms(raw_result)
         image, centers = detector.draw_boxes_and_centers(image, boxes, classes)
         cv2.imshow("nanodet_image", image)
